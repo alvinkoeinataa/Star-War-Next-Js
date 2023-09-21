f49075ed304a3efe344ab6ed6e579989b182cb5d
Alvin <alvinkoeinata@gmail.com>
2023-09-21 14:24:10 +0700

    update letak gambar

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,16 +6,18 @@ import Vehicle from "./vehicle";
 const Home = () => {
   return (
     <div>
-      <div style={{ position: "relative" }}>
-        <img className="h-auto max-w-full" src="/warr.png" alt="image description" />
-
-        <div className=" flex items-center justify-center" style={{ position: "absolute", top: "0", left: "0", right: "0", bottom: "0" }}>
-          <div className="bg-white p-6">
+      <div>
+        <div className=" flex items-center justify-center bg-black">
+          <div className="p-6 ">
             <Link href="/vehicle">
-              <h1 className="text-4xl font-bold cursor-pointer hover:underline hover:text-blue-500">Welcome to Star War Vehicle</h1>
+              <h1 className="text-white text-3xl font-bold cursor-pointer hover:underline hover:text-blue-500">Welcome to Star War Vehicle</h1>
             </Link>
           </div>
         </div>
+
+        <img className="h-auto max-w-full" src="/warr.png" alt="image description" />
+        <img className="h-auto max-w-full" src="/atbt.webp" alt="image description" />
+        <img className="h-auto max-w-full" src="/starr.jpg" alt="image description" />
       </div>
     </div>
   );
